cf7c5c67fa38aed19343a095fc55348744059f89
Nick D <ndeoudes@terpmail.umd.edu>
2022-11-04 18:06:04 +0100

    deez

--- handleLogIn.js
+++ handleLogIn.js
@@ -189,3 +189,20 @@ function changeFill(id){
 }
 
  
+function countdown(releaseDate,id){
+  var countDownDate = new Date(releaseDate).getTime();
+  var x = setInterval(function() {
+    var now = new Date().getTime();
+    var distance = countDownDate - now;
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    document.getElementById(id).innerHTML = days + "d " + hours + "h "
+    + minutes + "m " + seconds + "s ";
+    if (distance < 0) {
+      clearInterval(x);
+      document.getElementById("demo").innerHTML = "RELEASED";
+    }
+  }, 1000);
+  }
